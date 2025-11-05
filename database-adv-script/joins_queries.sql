dae108ed85f2b70e2933802bef101413c1a444ed
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-05 14:33:49 +0000

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,11 @@
+SELECT bookings.id, users.id, users.name, bookings.start_date, bookings.end_date
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.id;
 
+SELECT properties.id, properties.name, reviews.id, reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
+
+SELECT users.id, users.name, bookings.id, bookings.start_date
+FROM users
+FULL OUTER JOIN bookings ON users.id = bookings.user_id;
