7291ab8659475c1bbd82e44cd5df56ddbf99233b
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-05 14:48:05 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,14 +1,18 @@
--- Total number of bookings made by each user
-SELECT
-    user_id,
-    COUNT(*) AS total_bookings
-FROM bookings
-GROUP BY user_id;
+WITH user_bookings AS (
+    SELECT
+        user_id,
+        COUNT(*) AS total_bookings
+    FROM bookings
+    GROUP BY user_id
+),
+property_rank AS (
+    SELECT
+        property_id,
+        COUNT(*) AS total_bookings,
+        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+    FROM bookings
+    GROUP BY property_id
+)
 
--- Rank properties based on total bookings they have received
-SELECT
-    property_id,
-    COUNT(*) AS total_bookings,
-    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
-FROM bookings
-GROUP BY property_id;
+SELECT * FROM user_bookings;
+SELECT * FROM property_rank;
