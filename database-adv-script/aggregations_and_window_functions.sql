da7495d8f9ae5e5fdcd4a096a0d739106b87305c
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-05 14:42:35 +0000

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,8 +3,11 @@ FROM users
 JOIN bookings ON bookings.user_id = users.id
 GROUP BY users.id, users.name;
 
-SELECT properties.id, properties.name,
-       RANK() OVER(ORDER BY COUNT(bookings.id) DESC) AS property_rank
-FROM properties
-LEFT JOIN bookings ON bookings.property_id = properties.id
-GROUP BY properties.id, properties.name;
+SELECT id, name,
+       RANK() OVER(ORDER BY total_bookings DESC) AS property_rank
+FROM (
+    SELECT properties.id, properties.name, COUNT(bookings.id) AS total_bookings
+    FROM properties
+    LEFT JOIN bookings ON bookings.property_id = properties.id
+    GROUP BY properties.id, properties.name
+) AS t;
