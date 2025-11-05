6b1ce161e3b35a1cba6d98cc9ff42a8d25d5502e
Shadrack Okyere <masterokyere100@gmail.com>
2025-11-05 14:29:28 +0000

    Update joins_queries.sql

--- database-script-0x01/joins_queries.sql
+++ database-script-0x01/joins_queries.sql
@@ -1,26 +1,55 @@
--- Task 1 - Subqueries
+-- Task 0 - Complex Joins (MySQL compatible)
 
--- 1) Properties where average rating is greater than 4.0
+-- 1) INNER JOIN: all bookings with the users who made them
 SELECT
-    p.id,
-    p.name
-FROM properties p
-WHERE (
-    SELECT AVG(r.rating)
-    FROM reviews r
-    WHERE r.property_id = p.id
-) > 4.0;
-
-
--- 2) Correlated Subquery - Users with more than 3 bookings
-SELECT
-    u.id,
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date,
+    u.id AS user_id,
     u.first_name,
     u.last_name,
     u.email
+FROM bookings b
+INNER JOIN users u
+    ON b.user_id = u.id;
+
+
+-- 2) LEFT JOIN: all properties including those with NO reviews
+SELECT
+    p.id AS property_id,
+    p.name AS property_name,
+    r.id AS review_id,
+    r.rating,
+    r.comment
+FROM properties p
+LEFT JOIN reviews r
+    ON p.id = r.property_id;
+
+
+-- 3) FULL OUTER JOIN (MySQL workaround using UNION of LEFT + RIGHT JOIN)
+SELECT
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
 FROM users u
-WHERE (
-    SELECT COUNT(*)
-    FROM bookings b
-    WHERE b.user_id = u.id
-) > 3;
+LEFT JOIN bookings b
+    ON u.id = b.user_id
+
+UNION
+
+SELECT
+    u.id AS user_id,
+    u.first_name,
+    u.last_name,
+    b.id AS booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM users u
+RIGHT JOIN bookings b
+    ON u.id = b.user_id;
